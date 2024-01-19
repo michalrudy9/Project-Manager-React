f5b648a22cc89b1343ff7d12a9b8b67eb0dcda64
michalrudy9 <michallrudy9@gmail.com>
2024-01-19 14:47:52 +0100

    Added add new project feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {projectState.selectedProjectId === null ? <NewProject /> : ""}
+      {projectState.selectedProjectId === undefined ? (
+        <NoProjectSelected onStartAddProject={handleStartAddProject} /> ) : ( "" )}
     </main>
   );
 }
