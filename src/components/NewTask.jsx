f48cf4c4a0ea3eb2251a61be6db1be94ebcc055d
michalrudy9 <michallrudy9@gmail.com>
2024-01-21 13:17:29 +0100

    Provided ability to add tasks to project

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,8 +1,31 @@
-function NewTask() {
+import { useState } from "react";
+
+function NewTask({ onAdd, onDelete }) {
+  const [enteredTask, setEnteredTask] = useState();
+
+  function handleChange(event) {
+    setEnteredTask(event.target.value);
+  }
+
+  function handleClick() {
+    onAdd(enteredTask);
+    setEnteredTask("");
+  }
+
   return (
     <div className="flex itmes-center gap-4">
-      <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-      <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+      <input
+        type="text"
+        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        onChange={handleChange}
+        value={enteredTask}
+      />
+      <button
+        className="text-stone-700 hover:text-stone-950"
+        onClick={handleClick}
+      >
+        Add Task
+      </button>
     </div>
   );
 }
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,12 @@
 import Tasks from "./Tasks";
 
-function SelectedProject({ project, onDelete }) {
+function SelectedProject({
+  project,
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks,
+}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
@@ -25,7 +31,7 @@ function SelectedProject({ project, onDelete }) {
           {project.description}
         </p>
       </header>
-      <Tasks />
+      <Tasks tasks={tasks} onAdd={onAddTask} onDelete={onDeleteTask} />
     </div>
   );
 }
